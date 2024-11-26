bb11f5fbe63e633833edeb52abbd30058fe4f054
AmelSAT <aselatt079@g.educaand.es>
2024-11-26 13:15:24 +0100

    cambio 20 numeros a 30 numeros random

--- src/tarea3/tarea3.java
+++ src/tarea3/tarea3.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class tarea3 {
 	    public static void main(String[] args) {
 	        Random rand = new Random();
-	        for (int i = 0; i < 20; i++) {
+	        for (int i = 0; i < 30; i++) {
 	            System.out.println(rand.nextInt(10) + 1);
 	        }
 	    }
